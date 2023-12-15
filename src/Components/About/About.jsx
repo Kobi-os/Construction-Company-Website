fe61cedce1cb9620ac43a9c6aa44b2558b905b89
Wojtek <wojtek.kobier@wp.pl>
2023-12-15 15:54:45 +0100

    new animations

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import './about.scss'
+import { motion } from 'framer-motion'
 
 const About = () => {
   return (
-    <div className='about'>
-      <h1>O MNIE</h1>
-      <p>Firma Marek Dobrzyl z powodzeniem od prawie 20 lat realizuje najbardziej wymagające zlecenia w branży Remontowo-Budowlanej. 
+
+
+    <motion.div className='about'
+    initial={{ opacity: 0, y: 50 }}
+    animate={{ opacity: 1, y: 0 }}
+    transition={{ duration: 0.7 }}
+    >
+    <h1 className='title'>O MNIE</h1>
+      <p className='param'>Firma Marek Dobrzyl z powodzeniem od prawie 20 lat realizuje najbardziej wymagające zlecenia w branży Remontowo-Budowlanej. 
         Na podstawie gotowych projektów wykańczamy z najwiekszą starannością i dbałością o szczegóły. To w nich tkwi diabeł ;) 
         Doświadczenie zdobyte na rynku przekłada się na jakośc wykonanych prac i zaufanie naszych klientów, 
-        Co otwiera drogi do pozyskania nowych. Bardzo się z tego faktu cieszymy, jednocześnie zapraszamy do współpracy </p>
-    </div>
+        Co otwiera drogi do pozyskania nowych. Bardzo się z tego faktu cieszymy, jednocześnie zapraszamy do współpracy 
+      </p>
+    </motion.div>
+
   )
 }
 
